74a51a9e5d487006e5128ba171cc90a6b2a8fa31
Patrick Delaney <patrick.del@gmail.com>
2022-04-11 13:45:16 -0500

    update.

--- TODO.md
+++ TODO.md
@@ -1,6 +1,28 @@
 # 
 
-1. make sure pigpio installs in the proper location
-2. rasperry pi settings
-3. raspberry pi pw
+## Solar, Power Electronics Tasks
+
+0. Find power supply,  test output of converter circuit to ensure safe for Rpi.
+1. Create voltage divider circuit with safety diodes for Rpi ADC measurement unit.
+2. Add safety diodes to converter circuit to ensure safety for power out to Rpi off of USB.
+3. Create mathematical analog from GPIO measured input to actual voltage measurement.
+4. Find lead acid battery appropriate for test.
+5. Find solar panel appropriate for test.
+6. Calculate approximate needed solar panel and battery size for predicted load.
+
+
+## Software Tasks
+
+0. Organize pi setup script so that all software programs are installed in order, in the locations that we want,
+including setting the proper path variable so that the commands we want to envoke get envoked.
+And so that all system commands get started upon startup.
+
+1. Organize Pi configuration so that we have our Pi configured as we want upon Rpilite installation.
+
+2. Create fresh start / delete script.
+
+3. Create python script to read GPIO pins and write to database from Rpi.
+
+4. Create script which deletes all settings from above script in 0. so that when new setup script is run, we get a completely fresh setup.
+
 
--- setupngrok.sh
+++ setupngrok.sh
@@ -3,51 +3,78 @@
 
 # need to add another user to work with this, as well as a different password to obscure ngrok capability!
 
-
-# install ngrok with appropriate link to ARM version
-
-
-# unzip to folder within designated path
-
-
-set PATH variable
-
-/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
-
-# ensure Ngrok is in one of the folders in path
+# ---------- Setting Up Folder Structure ----------
+# create our structure, folder and set PATH
+# make a /home/bin directory for infrequently changed home binaries and executables
+sudo mkdir /home/bin
+# make a /home/app directory for any applications
+sudo mkdir /home/app
 
 
+# ---------- Setting PATH Variable ----------
+# set path variable to include /home/app and /home/bin, as well as put within bash profile.
+# PATH = /home/app:/home/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+# export /home/app:/home/bin into default $ PATH
+sudo echo export PATH="/home/app:/home/bin/:$PATH" >> ~/.bashrc
+# restart and source bash profile. We have to use interactive mode to run this command because /.bashrc in raspbian has a conditional
+# which explicitly stops processing if the shell is found to be non-interactive.
+# https://stackoverflow.com/questions/43659084/source-bashrc-in-a-script-not-working 
+
+. ~/.bashrc
+
+# otherwise the following has to be appended to ~/.bashrc
+# load user specific BASH configuration files
+# if [ -f $HOME/.bash_profile ]; then
+#   . $HOME/.bash_profile
+# fi
+# see https://raspberrypi.stackexchange.com/questions/59048/raspberry-pi-not-loading-bashrc
+
+
+# ---------- Setting Up Environment ----------
 # read environmental variable file for ngrok
+# note - in future, we should put this into a /home/source/rpizwsolar folder as our official git read source
+# cd /home/pi/Projects/rpizwsolar
+# while loop to read .env file and export
 
-# export the environmental variable for ngrok
 
-export NGROK_AUTHTOKEN
+# ---------- Installing ngrok to Desired Location ----------
+# install ngrok to /home/bin
+# Update 
+sudo apt-get update -y && sudo apt-get upgrade -y
+# go to tmp folder
+cd /tmp/
+# download ngrok zip file for arm version 
+sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+# unzip file
+sudo unzip ngrok-stable-linux-arm.zip
+# move ngrok to /home/bin. Note, ~/ is root, which is set to /home/pi
+sudo mv /tmp/ngrok /home/bin/ngrok
+# delete ngrok zip file
+sudo rm /tmp/ngrok-stable-linux-arm.zip
 
+# -----> Set ngrok settings
+
+# export NGROK_AUTHTOKEN included above
 # alternatively, putting everything into ngrok.yml
 # ngrok.yml sits in same folder parallel to ngrok
 # authtoken:
 # log: /var/log/ngrok.log
 # etc
-
-
-
 # run ngrok authtoken $NGROK_AUTHTOKEN after exported
 
 
+# ---------- Ngrok System Control Commands ----------
+
 # start on startup
 
-sudo systemctl daemon-reload
+# sudo systemctl daemon-reload
 
-sudo systemctl enable ngrok
+# sudo systemctl enable ngrok
 
-sudo systemctl start ngrok
+# sudo systemctl start ngrok
 
-sudo systemctl enable ssh
+# sudo systemctl enable ssh
 
-sudo systemctl start sshd
+# sudo systemctl start sshd
 
-
-./ngrok tcp 22
-
-
-# on the login side, need to 
+# call ngrok keepalive script
